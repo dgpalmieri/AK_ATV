9097ceb764a9cbd68beed812c2b538e159d3ac79
Tailon <tailrusse2020@gmail.com>
2021-04-13 21:11:28 -0800

    Updated doxygen comments to be in the qt style

--- AK_ATV_Simulator/Assets/Scripts/PauseMenu.cs
+++ AK_ATV_Simulator/Assets/Scripts/PauseMenu.cs
@@ -9,12 +9,16 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false; /*!< Flag for checking if the game is paused */
-    public bool inOptionsMenu = false; /*!< Flag for if user is in the options menu */
-    public GameObject pauseMenuUI; /*!< The object for the pause menu UI. This is assigned in Unity. */
-    public GameObject optionsUI; /*!< The object for the options UI. This is assigned in Unity. */
+    /*!< Flag for checking if the game is paused */
+    public static bool GameIsPaused = false;
+    /*!< Flag for if user is in the options menu */
+    public bool inOptionsMenu = false;
+    /*!< The object for the pause menu UI. This is assigned in Unity. */
+    public GameObject pauseMenuUI;
+    /*!< The object for the options UI. This is assigned in Unity. */
+    public GameObject optionsUI; 
 
-    //! Update is called once per frame
+    /*! Update is called once per frame */
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.P)){
@@ -27,21 +31,21 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    //! Returns user back to the game 
+    /*! Returns user back to the game */ 
     public void Resume(){
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
     
-    //! Sends user to the pause menu
+    /*! Sends user to the pause menu */
     void Pause(){
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
 
-    //! Makes the options in the options menu appear
+    /*! Makes the options in the options menu appear */
     public void LoadOptions(){
         inOptionsMenu = true;
         GameIsPaused = true;
@@ -50,26 +54,26 @@ public class PauseMenu : MonoBehaviour
         optionsUI.SetActive(true);
     }
 
-    //! Allows the user to go back to the pause menu from the optios menu
+    /*! Allows the user to go back to the pause menu from the optios menu */
     public void BackButton(){
         optionsUI.SetActive(false);
         pauseMenuUI.SetActive(true);
         inOptionsMenu = false;
     }
 
-    //! Loads the menu scene
+    /*! Loads the menu scene */
     public void LoadMenu(){
         Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
     }
 
-    //! Allows users to quit the game
+    /*! Allows users to quit the game */
     public void QuitGame(){
         Debug.Log("Quitting game");
         Application.Quit();
     }
 
-    //! Allows users to change the graphics quality settings 
+    /*! Allows users to change the graphics quality settings */
      public void SetQuality(int qualityIndex){
         QualitySettings.SetQualityLevel(qualityIndex);
     }
